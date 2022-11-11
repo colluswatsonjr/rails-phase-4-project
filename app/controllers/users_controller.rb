36db95d2aacd82370a08124ab7315ec79c2f7f19
Collus Watson <cklwatsonjr@gmail.com>
2022-11-10 21:18:43 -0500

    'add model macros to create realtionships'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
         users = User.all
         render json: users
     end
+    def show
+        user = User.find_by(id:params[:id])
+        render json: user
+    end
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: user, status: 201
+        else
+            render json: { errors: user.errors.full_message}, status: 422
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
+    end
 end
